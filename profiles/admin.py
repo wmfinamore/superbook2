e5876a3e839cc7638cf9234e74e120cd31c0c012
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-05-02 11:23:03 -0300

    Registered Profile in Admin module

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Profile
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class UserProfileInLine(admin.StackedInline):
+    model = Profile
+
+
+class NewUserAdmin(UserAdmin):
+    inlines = [UserProfileInLine]
+
+
+admin.site.unregister(User)
+admin.site.register(User, NewUserAdmin)
