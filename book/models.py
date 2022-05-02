8ad58c2d3c62de270c3f363fbe87fd331c194ada
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-05-02 10:58:53 -0300

    Refactored to use signals in a alternative location in the project

--- book/models.py
+++ book/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from config import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 # Model Mixin
@@ -54,12 +52,3 @@ class Profile(TimeStampedModel):
 
     def __str__(self):
         return self.user
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_profile_handler(sender, instance, created, **kwargs):
-    if not created:
-        return
-    # Create the profile object, only if it is newly created
-    profile = Profile(user=instance)
-    profile.save()
--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # local
-    'book',
+    'book.apps.BookConfig',
 ]
 
 MIDDLEWARE = [
