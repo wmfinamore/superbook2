9c99ce98e91c169eabcc46d03f2ce7eac9c5c12b
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-05-04 10:12:58 -0300

    Implemented simple example of class based view

---
+++ book/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import SuperVillain
+
+
+urlpatterns = [
+    path('hello-su/<str:name>/', SuperVillain.as_view()),
+    path('hello-su'/, SuperVillain.as_view()),
+]
--- book/views.py
+++ book/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
 
-# Create your views here.
+
+class GreetView(View):
+    greeting = "Hello {}!"
+    default_name = "World"
+
+    def get(self, request, **kwargs):
+        name = kwargs.pop("name", self.default_name)
+        return HttpResponse(self.greeting.format(name))
+
+
+class SuperVillain(GreetView):
+    greeting = "We are the future, {}. Not them."
+    default_name = "my friend"
